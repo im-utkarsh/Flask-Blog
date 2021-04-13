b8aa6fdd4f0c0f9f205e7a180e0430acfc732daa
im-utkarsh <kuutkarsh11@gmail.com>
2021-04-13 20:19:02 +0530

    connected Register functionality with database

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "4948d2d8b93d58cb96200fb3d8ba4593"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 
 from flaskblog import routes
