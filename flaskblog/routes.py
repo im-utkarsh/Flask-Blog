26ba017bc9e1c8a11ed15ba6c83408aeaf40a1f0
im-utkarsh <kuutkarsh11@gmail.com>
2021-04-15 19:56:53 +0530

    create, update and delete posts

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,31 +1,17 @@
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 from PIL import Image
 import secrets
 import os
 
-posts = [
-    {
-        "author": "Utkarsh Chauhan",
-        "title": "Blop Post 1",
-        "content": "First post content",
-        "date_posted": "April 5, 2021",
-    },
-    {
-        "author": "Jane Doe",
-        "title": "Blop Post 2",
-        "content": "Second post content",
-        "date_posted": "April 20, 2021",
-    },
-]
-
 
 @app.route("/")
 @app.route("/home")
 def home():
+    posts = Post.query.all()
     return render_template("home.html", posts=posts)
 
 
@@ -80,6 +66,11 @@ def logout():
 
 
 def save_picture(form_picture):
+    prev_picture = os.path.join(
+        app.root_path, "static/profile_pics", current_user.image_file
+    )
+    if os.path.exists(prev_picture) and os.path.basename(prev_picture) != "default.jpg":
+        os.remove(prev_picture)
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
@@ -116,3 +107,58 @@ def account():
     return render_template(
         "account.html", title="Account", image_file=image_file, form=form
     )
+
+
+@app.route("/post/new", methods=["GET", "POST"])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(
+            title=form.title.data, content=form.content.data, author=current_user
+        )
+        db.session.add(post)
+        db.session.commit()
+        flash("Post has been created!", "success")
+        return redirect(url_for("home"))
+    return render_template("create_update_post.html", title="New Post", form=form)
+
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template("post.html", title=post.title, post=post, legend="New Post")
+
+
+@app.route("/post/<int:post_id>/update", methods=["GET", "POST"])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        if form.title.data != post.title or form.content.data != post.content:
+            post.title = form.title.data
+            post.content = form.content.data
+            db.session.commit()
+            flash("Post updated Successfully!", "success")
+            return redirect(url_for("post", post_id=post.id))
+    elif request.method == "GET":
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template(
+        "create_update_post.html", title="Update Post", form=form, legend="Update Post"
+    )
+
+
+@app.route("/post/<int:post_id>/delete", methods=["POST"])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash("Post deleted Successfully!", "success")
+    return redirect(url_for("home"))
