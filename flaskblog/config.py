643e9c6ae7a275371643b95b2217d1124b8a7221
im-utkarsh <kuutkarsh11@gmail.com>
2021-04-19 15:12:25 +0530

    heroku only

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     SECRET_KEY = "4948d2d8b93d58cb96200fb3d8ba4593"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
+    SQLALCHEMY_DATABASE_URI = "postgres://qoetapoefbudwg:e5c808a162fb657f8b099adf6cfdd7c9343ff59327c9f1ae2f40385d651f63a9@ec2-34-206-8-52.compute-1.amazonaws.com:5432/d553m6arlp463b"
     MAIL_SERVER = "smtp.googlemail.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
