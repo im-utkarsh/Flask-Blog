fad5b428eb25ec688059df923e5b990ae9c5c1d0
im-utkarsh <kuutkarsh11@gmail.com>
2021-04-21 04:41:49 +0530

    database

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     SECRET_KEY = "4948d2d8b93d58cb96200fb3d8ba4593"
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
     MAIL_SERVER = "smtp.googlemail.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
